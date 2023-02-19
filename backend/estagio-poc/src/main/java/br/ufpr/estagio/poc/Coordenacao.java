7efa713da7fb97b033fc1c6b7ce20cc16976279d
Gabriel Volkof Mayer <gabrielvmayer@gmail.com>
2023-02-19 19:24:22 -0300

    add classes Coordenacao e TermoDeEstagio

---
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/poc/Coordenacao.java
@@ -0,0 +1,57 @@
+package br.ufpr.estagio.poc;
+
+import br.ufpr.estagio.poc.TermoDeEstagio;
+
+public class Coordenacao {
+	private int id;
+	private String cpf;
+	private String curso; // Estou satisfeito só com o nome do curso
+	private TermoDeEstagio termoDeEstagio;
+
+	public Coordenacao() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Coordenacao(int id, String cpf, String curso, TermoDeEstagio termoDeEstagio) {
+		super();
+		this.id = id;
+		this.cpf = cpf;
+		this.curso = curso;
+		this.termoDeEstagio = termoDeEstagio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public TermoDeEstagio getTermoDeEstagio() {
+		return termoDeEstagio;
+	}
+
+	public void setTermoDeEstagio(TermoDeEstagio termoDeEstagio) {
+		this.termoDeEstagio = termoDeEstagio;
+	}
+	
+	
+}
