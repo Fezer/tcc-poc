e3821baad4fc4dae77b2fb54814a1105185f5403
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2023-02-20 21:30:26 -0300

    Endpoint aluno implementado
    Realizada a implementação do endpoint aluno. Então, quando o front fizer uma requisição para o endpoint aluno passando como parametro o GRR, o back irá fazer uma chamada para a API do SIGA para recuperar as informações do aluno da API do Siga.

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/poc/controller/AlunoPocREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/poc/controller/AlunoPocREST.java
@@ -1,5 +1,51 @@
 package br.ufpr.estagio.poc.controller;
 
-public class AlunoPocREST {
+import java.net.URI;
+import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.ufpr.estagio.poc.exception.PocException;
+import br.ufpr.estagio.poc.model.AlunoPocDTO;
+import br.ufpr.estagio.poc.model.TermoPoc;
+import br.ufpr.estagio.poc.model.TermoPocDTO;
+
+@CrossOrigin
+@RestController
+public class AlunoPocREST {
+	
+	@Autowired
+	private ModelMapper mapper;
+	
+	@GetMapping("/aluno/{grr}")
+	public ResponseEntity<AlunoPocDTO> listarTermo(@PathVariable String grr){
+		try {
+		if(grr.isBlank() || grr.isEmpty()) {
+			throw new PocException(HttpStatus.BAD_REQUEST, "GRR não informado!");
+		} else {
+			RestTemplate restTemplate = new RestTemplate();
+			URI uri = UriComponentsBuilder.fromUriString("https://siga.ufpr.br:8380/siga/api/graduacao/discentes").queryParam("grr", grr).build().toUri();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "AquiVaiOToken");
+			ResponseEntity<AlunoPocDTO> aluno = restTemplate.getForEntity(uri, AlunoPocDTO.class);
+			return ResponseEntity.status(HttpStatus.OK).body(mapper.map(aluno, AlunoPocDTO.class));
+		}
+		}catch(PocException e) {
+			e.printStackTrace();
+			throw e;
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
+		}
+	}
 }
