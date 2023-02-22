291e788953f8d3c7a691a3793829994da1a71a2e
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2023-02-22 16:18:57 -0300

    Discente

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/poc/controller/AlunoPocREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/poc/controller/AlunoPocREST.java
@@ -16,6 +16,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.ufpr.estagio.poc.exception.PocException;
 import br.ufpr.estagio.poc.model.AlunoPocDTO;
+import br.ufpr.estagio.poc.model.Discente;
 
 @CrossOrigin
 @RestController
@@ -25,7 +26,7 @@ public class AlunoPocREST {
 	private ModelMapper mapper;
 	
 	@GetMapping("/aluno/{grr}")
-	public ResponseEntity<AlunoPocDTO> listarTermo(@PathVariable String grr){
+	public ResponseEntity<Discente> listarTermo(@PathVariable String grr){
 		try {
 			if(grr.isBlank() || grr.isEmpty()) {
 				throw new PocException(HttpStatus.BAD_REQUEST, "GRR não informado!");
@@ -33,9 +34,9 @@ public class AlunoPocREST {
 				RestTemplate restTemplate = new RestTemplate();
 				URI uri = UriComponentsBuilder.fromUriString("https://siga.ufpr.br:8380/siga/api/graduacao/discentes").queryParam("grr", grr).build().toUri();
 	            HttpHeaders headers = new HttpHeaders();
-	            headers.set("Authorization", "AquiVaiOToken");
-				AlunoPocDTO aluno = restTemplate.getForEntity(uri, AlunoPocDTO.class).getBody();
-				return ResponseEntity.status(HttpStatus.OK).body(mapper.map(aluno, AlunoPocDTO.class));
+	            headers.set("Authorization", "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoX3JoMTdNUG1rOWlZalZQMElHTnJwSzdsVlczN25GZ1J4TmFPMEcwZkk0In0.eyJleHAiOjE2NzcwOTE3MTAsImlhdCI6MTY3NzA5MTY1MCwiYXV0aF90aW1lIjoxNjc3MDkxNDY5LCJqdGkiOiI3Y2M5NzgyOS00MjhkLTQ3ZDQtYTE5Zi04Yjc1ZDNmZTBiZTgiLCJpc3MiOiJodHRwczovL2xvZ2luLnVmcHIuYnIvcmVhbG1zL21hc3RlciIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJmOjRhNTgyMGJjLWQ2MzMtNGMxZS1hYzhjLWRhOWRlNmRkY2I3OTptb3JhZXMxIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiZXN0YWdpb3MiLCJzZXNzaW9uX3N0YXRlIjoiZGFiMzMzM2EtMjgxYy00YjdjLTkyZTMtZjcwNzJmZTQ4MjM2IiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDAiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtbWFzdGVyIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiJkYWIzMzMzYS0yODFjLTRiN2MtOTJlMy1mNzA3MmZlNDgyMzYiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJMZW9uYXJkbyBNb3JhZXMiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJtb3JhZXMxIiwiZ2l2ZW5fbmFtZSI6Ikxlb25hcmRvIiwiZmFtaWx5X25hbWUiOiJNb3JhZXMiLCJlbWFpbCI6Im1vcmFlczFAdWZwci5iciJ9.epo9jx4ktQYF_LDT-Jhf_3viHrM_uDBwl2BqVHUBTyvo3NaxViC4Bz9SMRffBDYFyLoomZkTzl39UIGTGkBlUl1PiErzjvP_HhwHuxTzeZi9B0MGHqnIJ_U3QiH1QHqeG-N_Zl8H8bX7FQa6flDJ4_amk22lIbkyo-yQNWYQ57ia4Jh0f_dOV9WnpRuGS54DbK9mTFM9K6OCN7C-wyRsocWK1co5coIDnlIhoT-cv8em99fOuHRhclKD-UnZfWEAkmnt3TTFJnhGWwUWeYnit9HCSVssZlhz6RYPGMReKAU2ALs_fLcSygelsMPSdWpM74YtEw1i0Nniv_YQhMKlaA");
+				Discente discente = restTemplate.getForEntity(uri, Discente.class).getBody();
+				return ResponseEntity.status(HttpStatus.OK).body(mapper.map(discente, Discente.class));
 			}
 		}catch(PocException e) {
 			e.printStackTrace();
