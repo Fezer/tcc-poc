773a7f004cb5f6e026dca8669a33a6601ac46f94
Schwanka <gustavoschwanka@gmail.com>
2023-04-17 23:16:50 -0300

    Commit das listas de Processos para Coe, Coafe e coordenação.

---
+++ frontend/src/pages/coafe/coafeEmpresas.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="flex justify-content-between">
+    <h1>COAFE
+      <h6>Coordenação de Atividades Formativas e Estágios</h6>
+    </h1>
+    <span class="p-input-icon-left">
+        <Button label="Cadastrar"></Button>  
+    </span>    
+    </div>
+    <div>
+      <DataTable :value="Empresas" rowHover stripedRows>
+        <template #header>
+            <div class="flex justify-content-between">
+                <span class="p-input-icon-left">
+               <h4><b>Empresas</b></h4>
+            </span>
+                <span class="p-input-icon-left">
+                    <i class="pi pi-search" />
+                    <InputText placeholder="Keyword Search" />
+                </span>
+            </div>        
+        </template>
+        <template #empty> No customers found. </template>
+        <template #loading> Loading customers data. Please wait. </template>
+        <Column field="Empresas" header="Nome da Empresa">
+            <template #body="{ data }">
+                {{ data.empresa}}
+            </template>
+        </Column>
+        <Column field="cnpj" header="CNPJ">
+            <template #body="{ data }">
+                {{ data.cnpj }}
+            </template>
+        </Column>
+        <Column field="situação" header="Ativo">
+            <template #body="{ data }">
+                {{ data.situação }}
+            </template>
+        </Column>
+        <Column field="button">
+          <template #body>
+            <Button label="Ver"></Button>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+  </template>
+  
+  <script>
+  import Column from "primevue/column";
+  import DataTable from "primevue/datatable";
+  import Button from "primevue/button";
+  export default {
+    data() {
+      return {
+        Empresas:[{
+        empresa:'Empresa de Estágio 1',
+        cnpj:'124412142',
+        situação:'ATIVO',
+        },
+      {
+        empresa:'Empresa de Estágio 2',
+        cnpj:'89024908',
+        situação:'INATIVO',
+        }]
+      };
+    },
+  };
+  </script>
---
+++ frontend/src/pages/coafe/coafeRelatorios.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="flex justify-content-between">
+      <h1>COAFE
+        <h6>Coordenação de Atividades Formativas e Estágios</h6>
+      </h1>
+    </div>
+    <div class="flex justify-content-between">
+      <span class="p-input-icon-left">
+        <h3><b>Relatórios</b></h3>
+      </span>
+    </div>
+    <div>
+    <span class="p-input-icon-left">
+        <h5>Relatório de Estágios Ativos</h5>
+    </span>
+    <div class="card flex flex-wrap justify-content-center gap-3">
+        <div class="flex align-items-center">
+            <RadioButton  v-model="checked" inputId="opção1" name="tipoEstagio" value="estagioUfpr" :binary="true" />
+            <label for="opção1" class="ml-2"> Estágios UFPR </label>
+        </div>
+        <div class="flex align-items-center">
+            <RadioButton v-model="checked" inputId="opção2" name="tipoEstagio" value="estagioExterno" :binary="true" />
+            <label for="opção2" class="ml-2"> Estágios Externos </label>
+        </div>        
+    </div>
+  </div>
+</template>  
+<script>
+  import RadioButton  from 'primevue/radiobutton';
+  import Button from "primevue/button";
+ export default {
+    data() {
+      return {
+        Relatorios:[{
+        empresa:'Empresa de Estágio 1',
+        cnpj:'124412142',
+        situação:'ATIVO',
+        },
+      {
+        empresa:'Empresa de Estágio 2',
+        cnpj:'89024908',
+        situação:'INATIVO',
+        }]
+      };
+    },
+  };
+</script>
+<script setup>
+import { ref } from "vue";
+
+const checked = ref(false);
+</script>
