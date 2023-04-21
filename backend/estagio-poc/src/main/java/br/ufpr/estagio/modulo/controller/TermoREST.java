398bafc101521fd2e868b9d280196e25b43b26dd
Gabriel Mayer <gabrielvmayer@gmail.com>
2023-04-21 15:17:59 -0300

    adaptacao do que restou da poc

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/TermoREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/TermoREST.java
@@ -23,11 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.ufpr.estagio.modulo.dto.JustificativaDTO;
 import br.ufpr.estagio.modulo.dto.TermoDeEstagioDTO;
-import br.ufpr.estagio.modulo.dto.TermoPocDTO;
+import br.ufpr.estagio.modulo.enums.EnumStatusEstagio;
+import br.ufpr.estagio.modulo.enums.EnumStatusTermo;
 import br.ufpr.estagio.modulo.exception.PocException;
 import br.ufpr.estagio.modulo.model.TermoDeEstagio;
-import br.ufpr.estagio.modulo.model.TermoPoc;
-import br.ufpr.estagio.modulo.repository.TermoPocRepository;
 import br.ufpr.estagio.modulo.service.EstagioService;
 import br.ufpr.estagio.modulo.service.RelatorioDeEstagioService;
 import br.ufpr.estagio.modulo.service.TermoDeEstagioService;
@@ -48,9 +47,6 @@ public class TermoREST {
         this.termoDeEstagioService = termoDeEstagioService;
         this.relatorioDeEstagioService = relatorioDeEstagioService;
     }
-	
-	@Autowired
-	private TermoPocRepository repo;
 
 	@Autowired
 	private ModelMapper mapper;
@@ -148,20 +144,20 @@ public class TermoREST {
 	}
 	
 	@PutMapping("/aprovar/coafe/{id}")
-	public ResponseEntity<TermoPocDTO> aprovarCoafe(@PathVariable Long id){
+	public ResponseEntity<TermoDeEstagioDTO> aprovarCoafe(@PathVariable Long id){
 		try {
-			Optional<TermoPoc> termofind = repo.findById(id);
+			Optional<TermoDeEstagio> termofind = Optional.ofNullable(termoDeEstagioService.buscarPorId(id));
 			if(termofind.isEmpty()) {
 				throw new PocException(HttpStatus.NOT_FOUND, "Termo não encontrado!");
 			} else {
-				TermoPoc termo = new TermoPoc();
+				TermoDeEstagio termo = new TermoDeEstagio();
 				termo = termofind.get();
-				termo.setStatusTermo("Aprovado");
-				termo.setStatusEstagio("Aprovado");
-				termo.setParecerCOAFE("Aprovado");
-				repo.save(mapper.map(termo, TermoPoc.class));
-				termofind = repo.findById(id);
-				return ResponseEntity.status(HttpStatus.OK).body(mapper.map(termofind, TermoPocDTO.class));
+				termo.setStatusTermo(EnumStatusTermo.Aprovado);
+//				termo.setStatusEstagio(EnumStatusEstagio.Aprovado);
+//				termo.setParecerCOAFE("Aprovado");
+				termoDeEstagioService.salvar(mapper.map(termo, TermoDeEstagio.class));
+				termofind = Optional.ofNullable(termoDeEstagioService.buscarPorId(id));
+				return ResponseEntity.status(HttpStatus.OK).body(mapper.map(termofind, TermoDeEstagioDTO.class));
 			}
 		}catch(PocException e) {
 			e.printStackTrace();
@@ -173,25 +169,25 @@ public class TermoREST {
 	}
 	
 	@PutMapping("/reprovar/coafe/{id}")
-	public ResponseEntity<TermoPocDTO> reprovarCoafe(@PathVariable Long id, @RequestBody JustificativaDTO requestBody){
+	public ResponseEntity<TermoDeEstagioDTO> reprovarCoafe(@PathVariable Long id, @RequestBody JustificativaDTO requestBody){
 		try {
 			String justificativa = requestBody.getJustificativa();
-			Optional<TermoPoc> termofind = repo.findById(id);
+			Optional<TermoDeEstagio> termofind = Optional.ofNullable(termoDeEstagioService.buscarPorId(id));
 			if(termofind.isEmpty()) {
 				throw new PocException(HttpStatus.NOT_FOUND, "Termo não encontrado!");
 			} else {
 				if (justificativa.isBlank() || justificativa.isEmpty()){
 					throw new PocException(HttpStatus.BAD_REQUEST, "O motivo do indeferimento deve ser informado!");
 				} else {
-					TermoPoc termo = new TermoPoc();
+					TermoDeEstagio termo = new TermoDeEstagio();
 					termo = termofind.get();
-					termo.setStatusTermo("Reprovado");
-					termo.setStatusEstagio("Reprovado");
-					termo.setParecerCOAFE("Reprovado");
+					termo.setStatusTermo(EnumStatusTermo.Reprovado);
+//					termo.setStatusEstagio(EnumStatusEstagio.Reprovado);
+//					termo.setParecerCOAFE("Reprovado");
 					termo.setMotivoIndeferimento(justificativa);
-					repo.save(mapper.map(termo, TermoPoc.class));
-					termofind = repo.findById(id);
-					return ResponseEntity.status(HttpStatus.OK).body(mapper.map(termofind, TermoPocDTO.class));
+					termoDeEstagioService.salvar(mapper.map(termo, TermoDeEstagio.class));
+					termofind = Optional.ofNullable(termoDeEstagioService.buscarPorId(id));
+					return ResponseEntity.status(HttpStatus.OK).body(mapper.map(termofind, TermoDeEstagioDTO.class));
 				}
 			}
 		}catch(PocException e) {
