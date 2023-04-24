9c8013cb648ae8da6defef5db0f04ce354d4ea0f
Schwanka <gustavoschwanka@gmail.com>
2023-04-23 23:22:56 -0300

    Finalização da Página de Relatórios

--- frontend/src/pages/coafe/coafeRelatorios.vue
+++ frontend/src/pages/coafe/coafeRelatorios.vue
@@ -13,16 +13,19 @@
     <span class="p-input-icon-left">
         <h5>Relatório de Estágios Ativos</h5>
     </span>
-    <div class="card flex flex-wrap justify-content-center gap-3">
-        <div class="flex align-items-left">
-            <RadioButton  v-model="checked" inputId="opção1" name="tipoEstagio" value="estagioUfpr" :binary="true" />
+    <div class="card flex-column">
+      <div class="flex flex-row gap-10">
+        <div class="flex align-items-left pb-4">
+            <Checkbox  v-model="checked" inputId="opção1" name="tipoEstagio" value="estagioUfpr" :binary="true" />
             <label for="opção1" class="ml-2"> Estágios UFPR </label>
         </div>
-        <div class="flex align-items-left">
-            <RadioButton v-model="checked" inputId="opção2" name="tipoEstagio" value="estagioExterno" :binary="true" />
+        <div class="flex align-items-left pb-6">
+            <Checkbox v-model="checked" inputId="opção2" name="tipoEstagio" value="estagioExterno" :binary="true" />
             <label for="opção2" class="ml-2"> Estágios Externos </label>
         </div>        
-        <div class="flex align-items-center">
+      </div>
+      <div>
+        <div class="flex align-items-center pb-6 gap-6">
           <span class="p-float-label ml-2">            
             <Calendar v-model="date" dateFormat="dd/mm/yy" inputId="dataInicio" showIcon showButtonBar />
             <label for="dataInicio">De </label>
@@ -32,21 +35,57 @@
             <label for="DatadeFim">Até</label>
           </span>
         </div>
-        <div class="flex flex-column gap-2">
-            <label for="curso">Curso</label>
-            <InputText id="curso" v-model="value" />
-        </div>
+      </div>
+      <div class="flex flex-column gap-1">
+          <label for="curso">Curso</label>
+          <InputText id="curso" v-model="value" />
+      </div>
     </div>
-    <span class="p-input-icon-right">
-        <Button label="Cadastrar"></Button>  
-    </span>  
+    <div class="flex justify-content-end">
+        <Button label="Cadastrar" class="botao-principal"></Button>  
+    </div>
   </div>
+  <div>
+    <span class="p-input-icon-left">
+        <h5>Relatório de Indeferimentos</h5>
+    </span>
+    <div class="card flex-column">
+      <div class="flex flex-row gap-10">
+        <div class="flex align-items-left pb-4">
+            <Checkbox  v-model="checked" inputId="opção1" name="tipoEstagio" value="estagioUfpr" :binary="true" />
+            <label for="opção1" class="ml-2"> Estágios UFPR </label>
+        </div>
+        <div class="flex align-items-left pb-6">
+            <Checkbox v-model="checked" inputId="opção2" name="tipoEstagio" value="estagioExterno" :binary="true" />
+            <label for="opção2" class="ml-2"> Estágios Externos </label>
+        </div>        
+      </div>
+      <div>
+        <div class="flex align-items-center pb-6 gap-6">
+          <span class="p-float-label ml-2">            
+            <Calendar v-model="date" dateFormat="dd/mm/yy" inputId="dataInicio" showIcon showButtonBar />
+            <label for="dataInicio">De </label>
+          </span>
+          <span class="p-float-label ml-2">
+            <Calendar v-model="date" dateFormat="dd/mm/yy" inputId="DatadeFim" showIcon  showButtonBar />
+            <label for="DatadeFim">Até</label>
+          </span>
+        </div>
+      </div>
+      <div class="flex flex-column gap-1">
+          <label for="curso">Curso</label>
+          <InputText id="curso" v-model="value" />
+      </div>
+    </div>
+    <div class="flex justify-content-end">
+        <Button label="Cadastrar" class="botao-principal"></Button>  
+    </div>
+  </div>  
 </template>  
 <script>
-  import RadioButton  from 'primevue/radiobutton';
-  import Calendar from 'primevue/calendar';
   import InputText from "primevue/inputtext";
-  //import Button from "primevue/button";
+  import Button from "primevue/button";
+  import Checkbox from 'primevue/checkbox';
  export default {
     data() {
       return {
