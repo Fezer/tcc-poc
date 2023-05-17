8af4dd47ce512a4642d27687e414a31edae3cb9d
Gabriel Mayer <gabrielvmayer@gmail.com>
2023-05-17 18:23:40 -0300

    permite que a coordenação baixe um termo de compromisso upado pelo aluno

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/CoordenacaoREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/CoordenacaoREST.java
@@ -1,12 +1,19 @@
 package br.ufpr.estagio.modulo.controller;
 
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +27,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.ufpr.estagio.modulo.dto.DescricaoAjustesDTO;
 import br.ufpr.estagio.modulo.dto.JustificativaDTO;
 import br.ufpr.estagio.modulo.dto.TermoDeEstagioDTO;
+import br.ufpr.estagio.modulo.enums.EnumTipoDocumento;
+import br.ufpr.estagio.modulo.exception.BadRequestException;
 import br.ufpr.estagio.modulo.exception.NotFoundException;
 import br.ufpr.estagio.modulo.exception.PocException;
+import br.ufpr.estagio.modulo.model.Aluno;
 import br.ufpr.estagio.modulo.model.TermoDeEstagio;
+import br.ufpr.estagio.modulo.service.AlunoService;
 import br.ufpr.estagio.modulo.service.TermoDeEstagioService;
 
 @CrossOrigin
@@ -32,6 +43,9 @@ public class CoordenacaoREST {
 
 	@Autowired
 	private TermoDeEstagioService termoDeEstagioService;
+	
+	@Autowired
+	private AlunoService alunoService;
 		
 	@Autowired
 	private ModelMapper mapper;
@@ -253,6 +267,40 @@ public class CoordenacaoREST {
 			e.printStackTrace();
 			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
 		}
+	}
+	
+	@GetMapping("/{grrAlunoURL}/download-termo")
+	public ResponseEntity<Resource> downloadTermo(@PathVariable String grrAlunoURL) {
+	    if (grrAlunoURL.isBlank() || grrAlunoURL.isEmpty()) {
+	        throw new BadRequestException("GRR do aluno não informado!");
+	    } else {
+	        Aluno aluno = alunoService.buscarAlunoPorGrr(grrAlunoURL);
+	        if (aluno == null) {
+	            throw new NotFoundException("Aluno não encontrado!");
+	        } else {
+	        	Path diretorioAtual = Paths.get("").toAbsolutePath();
+            	String diretorioDestino = diretorioAtual + "/src/main/resources/arquivos/";
+            	
+	            String nomeArquivo = grrAlunoURL + "-" + EnumTipoDocumento.TermoDeCompromisso;
+	            Path arquivo = Paths.get(diretorioDestino + nomeArquivo);
+
+	            try {
+	                Resource resource = new UrlResource(arquivo.toUri());
+
+	                if (resource.exists()) {
+	                    return ResponseEntity.ok()
+	                    		.contentType(MediaType.APPLICATION_PDF)
+	                            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+	                            .body(resource);
+	                } else {
+	                    throw new NotFoundException("Arquivo não encontrado!");
+	                }
+	            } catch (MalformedURLException e) {
+	                e.printStackTrace();
+	                throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro ao obter o arquivo!");
+	            }
+	        }
+	    }
 	}
 
 }
