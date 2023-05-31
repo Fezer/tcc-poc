852428cefd64f89f68b7306b1b7812cafd1158cf
Gabriel Mayer <gabrielvmayer@gmail.com>
2023-05-31 17:17:01 -0300

    corrige validacoes de endereco

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/EnderecoREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/EnderecoREST.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufpr.estagio.modulo.dto.EnderecoDTO;
+import br.ufpr.estagio.modulo.dto.ErrorResponse;
 import br.ufpr.estagio.modulo.exception.InvalidFieldException;
 import br.ufpr.estagio.modulo.exception.NotFoundException;
 import br.ufpr.estagio.modulo.exception.PocException;
@@ -37,110 +38,169 @@ public class EnderecoREST {
 	private ModelMapper mapper;
 	
 	@PostMapping("/")
-	public ResponseEntity<EnderecoDTO> inserir(@RequestBody EnderecoDTO enderecoDTO){
+	public ResponseEntity<Object> inserir(@RequestBody EnderecoDTO enderecoDTO){
 		try {
 		
-			if (enderecoDTO.getRua().isBlank() || enderecoDTO.getRua().isEmpty())
-	    		throw new InvalidFieldException("Logradouro inválido.");
+			if (enderecoDTO.getRua() == null || enderecoDTO.getRua().isEmpty())
+	    		throw new InvalidFieldException("Rua inválida.");
 	    	
 	    	if (enderecoDTO.getNumero() < 1)
 	    		throw new InvalidFieldException("Número inválido.");
 	    	
-	    	if (enderecoDTO.getComplemento().isBlank() || enderecoDTO.getComplemento().isEmpty())
-	    		throw new InvalidFieldException("Complemento inválido.");
+	    	//if (enderecoDTO.getComplemento() == null || enderecoDTO.getComplemento().isEmpty())
+	    		//throw new InvalidFieldException("Complemento inválido.");
 	    	
-	    	if (enderecoDTO.getCidade().isBlank() || enderecoDTO.getCidade().isEmpty())
+	    	if (enderecoDTO.getCidade() == null || enderecoDTO.getCidade().isEmpty())
 	    		throw new InvalidFieldException("Cidade inválida.");
 	    	
-	    	if (enderecoDTO.getUf().isBlank() || enderecoDTO.getUf().isEmpty())
+	    	if (enderecoDTO.getUf() == null || enderecoDTO.getUf().isEmpty())
 	    		throw new InvalidFieldException("Estado inválido.");
 	    	
-	    	if (enderecoDTO.getCep().isBlank() || enderecoDTO.getCep().isEmpty())
+	    	if (enderecoDTO.getCep() == null || enderecoDTO.getCep().isEmpty())
 	    		throw new InvalidFieldException("CEP inválido.");
 			
 	    	// TO-DO: Validação de pessoa -> enderecoDTO.getPessoa()getCadaAtributo()...
 	    	
 		Endereco newEndereco = enderecoService.criarEndereco(mapper.map(enderecoDTO, Endereco.class));
 		return ResponseEntity.status(HttpStatus.CREATED).body(mapper.map(newEndereco, EnderecoDTO.class));
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
+		} catch (InvalidFieldException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
 	    } catch(Exception e) {
 			e.printStackTrace();
-			throw new NotFoundException("Não foi possível criar um novo endereço.");
+			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
 		}
 	}
 	
 	@GetMapping("/")
 	public ResponseEntity<List<EnderecoDTO>> listarTodos(){
-		
-		// o try estava sobrescrevendo o lançamento de NotFoundException. por algum motivo,
-		// a mesma estrutura, em outra classe, funciona mesmo com o try. vou verificar depois
-		//try {
-			List<Endereco> lista = enderecoService.listarEnderecos();
-			if(lista.isEmpty()) {
+
+		try {
+			List<Endereco> enderecos = enderecoService.listarEnderecos();
+			/*if(lista.isEmpty()) {
 				throw new NotFoundException("Nenhum endereço encontrado!");
 			} else {
 				return ResponseEntity.status(HttpStatus.OK).body(lista.stream().map(e -> mapper.map(e, EnderecoDTO.class)).collect(Collectors.toList()));
-			}
-		/*}catch(Exception e) {
+			}*/
+			List<EnderecoDTO> enderecosDTO = enderecos.stream()
+    	            .map(ap -> mapper.map(ap, EnderecoDTO.class))
+    	            .collect(Collectors.toList());
+    	    return ResponseEntity.ok().body(enderecosDTO);
+		}catch(Exception e) {
 			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
-		}*/
+		}
 	}
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<EnderecoDTO> listarEndereco(@PathVariable Long id){
+	public ResponseEntity<Object> listarEndereco(@PathVariable String id){
 		try {
-			Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(id);
-		if(enderecoFind.isEmpty()) {
-			throw new NotFoundException("Endereço não encontrado!");
-		} else {
-			Endereco endereco = enderecoFind.get();
-			EnderecoDTO enderecoDTO = mapper.map(endereco, EnderecoDTO.class);
-			return new ResponseEntity<>(enderecoDTO, HttpStatus.OK);
-		}
-		}catch(PocException e) {
-			e.printStackTrace();
-			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
-		}catch(Exception e) {
+			long idLong = Long.parseLong(id);
+	    	
+		    if (idLong < 1) {
+		   		throw new InvalidFieldException("Id do endereço inválido!");
+		   	}
+		    
+			Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(idLong);
+			
+			if(enderecoFind.isEmpty()) {
+				throw new NotFoundException("Endereço não encontrado!");
+			} else {
+				Endereco endereco = enderecoFind.get();
+				EnderecoDTO enderecoDTO = mapper.map(endereco, EnderecoDTO.class);
+				return new ResponseEntity<>(enderecoDTO, HttpStatus.OK);
+			}
+		} catch (NotFoundException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	    } catch (NumberFormatException ex) {
+	        ErrorResponse response = new ErrorResponse("Id do endereço deve ser um número!");
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch (InvalidFieldException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception e) {
 			e.printStackTrace();
 			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
 		}
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity<EnderecoDTO> atualizar(@PathVariable Long id, @RequestBody EnderecoDTO endereco){
+	public ResponseEntity<Object> atualizar(@PathVariable String id, @RequestBody EnderecoDTO endereco){
 		try {
-			Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(id);
-		if(enderecoFind.isEmpty()) {
-			throw new NotFoundException("Endereço não encontrado!");
-		} else {
-			Endereco enderecoAtualizado = enderecoService.atualizarEndereco(enderecoFind.get(), endereco);
-			return ResponseEntity.status(HttpStatus.OK).body(mapper.map(enderecoAtualizado, EnderecoDTO.class));
-		}
-		}catch(PocException e) {
-			e.printStackTrace();
-			throw e;
-		}catch(Exception e) {
+			long idLong = Long.parseLong(id);
+	    	
+		    if (idLong < 1) {
+		   		throw new InvalidFieldException("Id do endereço inválido!");
+		   	}
+		    
+			Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(idLong);
+			
+			if(enderecoFind.isEmpty()) {
+				throw new NotFoundException("Endereço não encontrado!");
+			} else {
+				if (endereco.getRua() == null || endereco.getRua().isEmpty())
+		    		throw new InvalidFieldException("Rua inválida.");
+	    	
+		    	if (endereco.getNumero() < 1)
+		    		throw new InvalidFieldException("Número inválido.");
+		    	
+		    	//if (enderecoDTO.getComplemento() == null || enderecoDTO.getComplemento().isEmpty())
+		    		//throw new InvalidFieldException("Complemento inválido.");
+		    	
+		    	if (endereco.getCidade() == null || endereco.getCidade().isEmpty())
+		    		throw new InvalidFieldException("Cidade inválida.");
+		    	
+		    	if (endereco.getUf() == null || endereco.getUf().isEmpty())
+		    		throw new InvalidFieldException("Estado inválido.");
+		    	
+		    	if (endereco.getCep() == null || endereco.getCep().isEmpty())
+		    		throw new InvalidFieldException("CEP inválido.");
+		    	
+				Endereco enderecoAtualizado = enderecoService.atualizarEndereco(enderecoFind.get(), endereco);
+				return ResponseEntity.status(HttpStatus.OK).body(mapper.map(enderecoAtualizado, EnderecoDTO.class));
+			}
+		} catch (NotFoundException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	    } catch (NumberFormatException ex) {
+	        ErrorResponse response = new ErrorResponse("Id do endereço deve ser um número!");
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch (InvalidFieldException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception e) {
 			e.printStackTrace();
 			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
 		}
 	}
 		
 	@DeleteMapping("/{id}")
-	public ResponseEntity<EnderecoDTO> delete(@PathVariable Long id){
+	public ResponseEntity<?> delete(@PathVariable String id){
 		try {
-			Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(id);
-		if(enderecoFind.isEmpty()) {
-			throw new NotFoundException("Endereço não encontrado!");
-		} else {
-			enderecoService.excluirEndereco(enderecoFind.get());
-			return ResponseEntity.status(HttpStatus.OK).body(null);
-		}
-		}catch(PocException e) {
-			e.printStackTrace();
-			throw e;
-		}catch(Exception e) {
+			long idLong = Long.parseLong(id);
+	    	
+		    if (idLong < 1) {
+		   		throw new InvalidFieldException("Id do endereço inválido!");
+		   	}
+		    
+		    Optional<Endereco> enderecoFind = enderecoService.buscarEndercoPorId(idLong);
+		    
+			if(enderecoFind.isEmpty()) {
+				throw new NotFoundException("Endereço não encontrado!");
+			} else {
+				enderecoService.excluirEndereco(enderecoFind.get());
+				return ResponseEntity.status(HttpStatus.OK).body(null);
+			}
+		} catch (NotFoundException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+	    } catch (NumberFormatException ex) {
+	        ErrorResponse response = new ErrorResponse("Id do endereço deve ser um número!");
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch (InvalidFieldException ex) {
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception e) {
 			e.printStackTrace();
 			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
 		}
