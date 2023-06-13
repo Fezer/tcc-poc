fedd93ce0937334e4a175b761174000634ef05ff
Lucas Zawadneak <lucaszawadneak@gmail.com>
2023-06-13 19:47:28 -0300

    backend: fix erro 401

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/keycloak/SecurityConfig.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/keycloak/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login")
                         .permitAll()
                         .anyRequest()
-                        .authenticated());
+                        .permitAll());
 
         // http.authorizeHttpRequests(requests -> requests
         // .requestMatchers("/siga*")
