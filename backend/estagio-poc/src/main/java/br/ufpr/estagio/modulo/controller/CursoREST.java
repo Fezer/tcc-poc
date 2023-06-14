03c5b868de7c6f9c0c5ea26ad537118fa6d5f109
Gabriel Mayer <gabrielvmayer@gmail.com>
2023-06-14 11:50:34 -0300

    corrige validacao de curso

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/CursoREST.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/controller/CursoREST.java
@@ -55,12 +55,14 @@ public class CursoREST {
 		} catch (NoSuchElementException ex) {
 	        ErrorResponse response = new ErrorResponse("Curso não encontrado!");
 	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-	    } catch (PocException e) {
-			e.printStackTrace();
-			throw e;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
+	    } catch (RuntimeException ex) {
+			ex.printStackTrace();
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception ex) {
+	    	ex.printStackTrace();
+	    	ErrorResponse response = new ErrorResponse("Desculpe, mas um erro inesperado ocorreu e não possível processar sua requisição.");
+	    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
 	}
     
@@ -79,28 +81,32 @@ public class CursoREST {
 		} catch (NoSuchElementException ex) {
 	        ErrorResponse response = new ErrorResponse("Curso não encontrado!");
 	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
-	    } catch (PocException e) {
-			e.printStackTrace();
-			throw e;
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
+	    } catch (RuntimeException ex) {
+			ex.printStackTrace();
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception ex) {
+	    	ex.printStackTrace();
+	    	ErrorResponse response = new ErrorResponse("Desculpe, mas um erro inesperado ocorreu e não possível processar sua requisição.");
+	    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
 	}
 
 	@GetMapping("")
-	public ResponseEntity<List<CursoDTO>> listarCursos() {
+	public ResponseEntity<Object> listarCursos() {
 		try {
 		    List<Curso> listaCursos = cursoService.listarTodosCursos();
 			
 		    return ResponseEntity.status(HttpStatus.OK).body(listaCursos.stream().map(e -> mapper.map(e, CursoDTO.class)).collect(Collectors.toList()));
 			
-		}catch(PocException e) {
-			e.printStackTrace();
-			throw e;
-		}catch(Exception e) {
-			e.printStackTrace();
-			throw new PocException(HttpStatus.INTERNAL_SERVER_ERROR, "Erro!");
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+	        ErrorResponse response = new ErrorResponse(ex.getMessage());
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+	    } catch(Exception ex) {
+	    	ex.printStackTrace();
+	    	ErrorResponse response = new ErrorResponse("Desculpe, mas um erro inesperado ocorreu e não possível processar sua requisição.");
+	    	return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
 	}
 	
