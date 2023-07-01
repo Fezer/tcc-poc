7d2183b6c00f105d05d18dbb50fe8c81ef6d926a
Gabriel Mayer <gabrielvmayer@gmail.com>
2023-07-01 12:12:02 -0300

    back: adiciona titulos as planilhas

--- backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/service/GeradorDeExcelService.java
+++ backend/estagio-poc/src/main/java/br/ufpr/estagio/modulo/service/GeradorDeExcelService.java
@@ -6,6 +6,9 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -25,18 +28,32 @@ public class GeradorDeExcelService {
 		
 	    try (Workbook workbook = new XSSFWorkbook()) {
 			Sheet sheet = workbook.createSheet("Relatório Estágios");
-
+			
 			String[] headersTitle = {"Id do Estágio", "Nome Aluno", "GRR", "IRA", "Curso", "Status do Estágio", "Contratante", 
 					"Seguradora", "Apólice", "Orientador", "Agente Integrador", "Supervisor", "Data de Início", "Data de Término",
 					"Jornada Diária", "Jornada Semanal", "Valor da Bolsa", "Valor de Transporte", "Rua do Estágio", "Número",
 					"Cidade", "Estado", "CEP"};
-			Row headerRow = sheet.createRow(0);
+			
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Estágios com Seguradora UFPR");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			for (Estagio estagio : estagios) {
 			    Row row = sheet.createRow(rowNum++);
 			    String dataInicio = new SimpleDateFormat("dd/MM/yyyy").format(estagio.getDataInicio());
@@ -85,13 +102,26 @@ public class GeradorDeExcelService {
 			String[] headersTitle = {"Nome", "CNPJ", "CPF", "Representante", "Telefone", "Rua", "Número", "Cidade", 
 					"Estado", "CEP", "Quantidade de Estágios"};
 			
-			Row headerRow = sheet.createRow(0);
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Contratante");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			
 			Row row = sheet.createRow(rowNum++);
 			row.createCell(0).setCellValue(contratante.getNome());
@@ -119,13 +149,26 @@ public class GeradorDeExcelService {
 			Sheet sheet = workbook.createSheet("Relatório Agente Integrador");
 
 			String[] headersTitle = {"Nome", "CNPJ", "Telefone", "qtdConvenios", "qtdEstagios", "ativo"};
-			Row headerRow = sheet.createRow(0);
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Agente Integrador");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			
 			Row row = sheet.createRow(rowNum++);
 			row.createCell(0).setCellValue(agenteIntegrador.getNome());
@@ -150,13 +193,26 @@ public class GeradorDeExcelService {
 			String[] headersTitle = {"Id", "Estagiário", "Orientador", "Contratante", "Data de Início", "Data de Término", 
 					"Total de Horas", "Etapa do Fluxo", "Parecer COE", "Motivo da Reprovação"};
 			
-			Row headerRow = sheet.createRow(0);
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Certificados de Estágio");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			for (CertificadoDeEstagio certificado : certificados) {
 			    Row row = sheet.createRow(rowNum++);
 			    String dataInicio = new SimpleDateFormat("dd/MM/yyyy").format(certificado.getEstagio().getDataInicio());
@@ -195,13 +251,26 @@ public class GeradorDeExcelService {
 					"Contribuição no Estágio", "Desenvolvimento de Atividades", "Efetivação", "Formação Profissional",
 					"Relações Interpessoais", "Considerações", "Seguradora", "Apólice", "Orientador", "Agente Integrador", 
 					"Supervisor", "Data de Início", "Data de Término", "Rua do Estágio", "Número", "Cidade", "Estado", "CEP"};
-			Row headerRow = sheet.createRow(0);
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Relatórios de Estágio");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			for (RelatorioDeEstagio relatorio : relatorios) {
 			    Row row = sheet.createRow(rowNum++);
 			    
@@ -251,13 +320,26 @@ public class GeradorDeExcelService {
 					"Contribuição no Estágio", "Desenvolvimento de Atividades", "Efetivação", "Formação Profissional",
 					"Relações Interpessoais", "Considerações", "Seguradora", "Apólice", "Orientador", "Agente Integrador", 
 					"Supervisor", "Data de Início", "Data de Término", "Rua do Estágio", "Número", "Cidade", "Estado", "CEP"};
-			Row headerRow = sheet.createRow(0);
+			Row titleRow = sheet.createRow(0);
+			Cell titleCell = titleRow.createCell(0);
+			titleCell.setCellValue("Relatório de Estágios com Seguradora UFPR");
+
+			int numColumns = headersTitle.length;
+
+			CellRangeAddress mergedRegion = new CellRangeAddress(0, 0, 0, numColumns - 1);
+			sheet.addMergedRegion(mergedRegion);
+
+			CellStyle titleCellStyle = workbook.createCellStyle();
+			titleCellStyle.setAlignment(HorizontalAlignment.CENTER);
+			titleCell.setCellStyle(titleCellStyle);
+
+			Row headerRow = sheet.createRow(1);
 			for (int i = 0; i < headersTitle.length; i++) {
 			    Cell cell = headerRow.createCell(i);
 			    cell.setCellValue(headersTitle[i]);
 			}
 
-			int rowNum = 1;
+			int rowNum = 2;
 			Row row = sheet.createRow(rowNum++);
 			
 			String dataInicio = new SimpleDateFormat("dd/MM/yyyy").format(relatorio.getEstagio().getDataInicio());
